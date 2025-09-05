1133367cc5ef5dd4eb3401c76cc8f3a740db8a80
Giovanamvv <giovana-verao@outlook.com>
2025-09-04 21:54:06 -0300

    aula-java-html

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-alert("Olá mundo!")
+// alert("Olá mundo!")
+// alert("Oie")
+// alert("Tudo bem?")
+// alert("Você vem sempre aqui?")
+
+let idade
+idade = 18
+alert(idade)
+
+idade = "Melancia"
+alert(idade)
+
+idade = 18.1
+alert(idade)
+
+const amor = "Meu primeiro amor"
+alert(amor)
+
+alert(amor)
+let amor2 = "Meu segundo amor"
+
+var antigao = "Nunca mais usar está m*rda!"
+
+
+
