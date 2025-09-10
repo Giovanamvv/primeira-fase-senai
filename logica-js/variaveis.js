da1fbb24bfc37ef2a337baffe3cfb5813c71acaf
GIOVANA MARQUES VERÃO <giovana_verao@estudante.sesisenai.org.br>
2025-09-09 22:26:32 -0300

    variaveis em js

---
+++ logica-js/variaveis.js
@@ -0,0 +1,25 @@
+////////////////variável com var//////////////////////
+//var nome="Giovana"  //string
+// var nome="Jurandir"
+// nome="Severino"
+
+///////////////variável com let//////////////////////
+// let nome="Giovana"
+// nome= "Jurandir"
+
+///////////////variável com const//////////////////////
+const nome="Giovana" // string
+const sobrenome= "Verão" //string       
+const idade= 24  //number
+const feminino= true //boolean
+
+//array 
+const frutas= ["laranja", "manga", "banana"]
+
+console.log(frutas[0])
+
+//  console.log("Meu nome é: " + nome + " " + sobrenome + " ,e tenho  " + idade + " anos")
+
+// console.log(`Meu nome é ${nome} ${sobrenome} ,e tenho ${idade} anos`) //template string (crase)
+
+
