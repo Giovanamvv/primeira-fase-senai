533b98bb20eb09a6c8ad8529958d6509adaceea0
GIOVANA MARQUES VERÃO <giovana_verao@estudante.sesisenai.org.br>
2025-09-10 22:15:23 -0300

    exercicioJS

---
+++ exercicioSq02/ex01.js
@@ -0,0 +1,23 @@
+// alert("ex01 executando ok bem bom!")
+
+//  let idade
+// idade= Number(prompt("Digite sua idade:"))
+// if(idade>=18){
+//     alert("Maior de idade")
+//     alert("Pode sair para o intervalo")
+// }else{
+//     alert("Dimenó")
+//     alert("Pode sair pro recreio")
+// }
+// console.log(idade);
+
+
+//Ex04////////// %2==0-DIVISAO POR DOIS SE0PAR SE1IMPAR
+alert("ex04 ta prestando!")
+let numero
+numero= Number(prompt("Digite o número: "))
+if(numero%2==0){
+    alert("par")
+}else{
+    alert("ímpar")
+}
