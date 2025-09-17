7cbf789dbd0f38b7729c5e2ec9de6c0091dd8793
GIOVANA MARQUES VERÃO <giovana_verao@estudante.sesisenai.org.br>
2025-09-16 22:11:18 -0300

    arrays-objetos.js

---
+++ logica-js/arrays-objetos.js
@@ -0,0 +1,22 @@
+const alunosDoSenai = [
+    {
+        nome: "João",
+        email: "joao@gmail.com",
+        matricula: "1234567",
+        idade: 25
+    },
+    {
+        nome: "Maria",
+        email: "maria@gmail.com",
+        matricula: "1234568",
+        idade: 22
+    },
+    {
+        nome: "Giovana",
+        email: "giovana@gmail.com",
+        matricula: "17102001",
+        idade: 24
+    }
+]
+
+console.log(`Meu nome é ${alunosDoSenai[2].nome}, a minha matrícula é ${alunosDoSenai[2].matricula}, o meu e-mail é ${alunosDoSenai[2].email} e a minha idade é ${alunosDoSenai[2].idade}`)
---
+++ logica-js/objetos.js
@@ -0,0 +1,10 @@
+const usuario = {
+    login:"username",
+    email:"email@gmail.com",
+    saldo: 50000.00,
+    isLogin: true,
+    senha:"123456"
+}
+
+console.log(usuario.email)
+console.log(`O e-mail do usuário é ${usuario.email} e o saldo é ${usuario.saldo}`)
