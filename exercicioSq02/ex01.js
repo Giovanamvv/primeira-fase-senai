7c69aa0a4e9a5fc2a2f71751a0315b655c6923be
GIOVANA MARQUES VERÃO <giovana_verao@estudante.sesisenai.org.br>
2025-09-17 22:04:16 -0300

    Exercicios01ao5.6

--- exercicioSq02/ex01.js
+++ exercicioSq02/ex01.js
@@ -12,12 +12,116 @@
 // console.log(idade);
 
 
-//Ex04////////// %2==0-DIVISAO POR DOIS SE0PAR SE1IMPAR
-alert("ex04 ta prestando!")
-let numero
-numero= Number(prompt("Digite o número: "))
-if(numero%2==0){
-    alert("par")
-}else{
-    alert("ímpar")
-}
+// //Ex04////////// %2==0-DIVISAO POR DOIS SE0PAR SE1IMPAR
+// alert("ex04 ta prestando!")
+// let numero
+// numero= Number(prompt("Digite o número: "))
+// if(numero%2==0){
+//     alert("par")
+// }else{
+//     alert("ímpar")
+// }
+
+
+// ///Ex05.1//////////
+// let senha
+
+// senha = prompt("Digite a senha:")
+// if(senha == 1234){
+//     alert("ACESSO PERMITIDO")
+// }else{
+//     alert("Acesso negado 🚫")
+
+
+// }
+
+// //Ex05.2//////////
+// let A, B
+// A = Number(prompt("Digite o primeiro número:"))
+// B = Number(prompt("Digite o segundo número:"))
+// if(A > B){
+//     alert("Primeiro: "+ A )
+//     // alert("Primeiro ("+ A +") é maior que o segundo ("+ B +")")
+// }else if(B > A){
+//     alert("Segundo: "+ B)
+//     // alert("Segundo ("+ B +") é maior que o primeiro ("+ A +")")
+// }else{
+//     alert("São iguais")
+
+// }
+
+
+
+
+//Ex5.3//////////
+// let placa
+// placa= Number(prompt("Último número da placa:  "))
+
+// if(placa==0){
+//     alert("Não pode rodar ma segunda-feira")
+// }
+// if(placa==1){
+//     alert(" Não pode rodar na segunda-feira")
+// }
+//  //outro jeito//--->5.3
+//  let placa
+// placa= Number(prompt("Último número da placa:  "))
+
+// if(placa==0 || placa==1){
+//     alert("Não pode rodar na segunda-feira")
+// }else if(placa==2 || placa==3){
+//     alert("Não pode rodar na terça-feira")
+// }else if(placa==4 || placa==5){
+//     alert("Não pode rodar na quarta-feira")
+// }else if(placa==6 || placa==7){
+//     alert("Não pode rodar na quinta-feira")
+// }else if(placa==8 || placa==9){
+//     alert("Não pode rodar na sexta-feira")
+// }else{
+//     alert("Sai do celular e digita direito")
+// }
+
+
+
+// //Ex5.4//////////
+// let  horario, local, palestra, nome
+// palestra= Number(prompt("Número da palestra:"))
+// if(palestra==1){
+//    alert("Animações com Scratch, 19h, Laboratório 305")
+   
+// }else if(palestra==2){
+//    alert ("Scratch para gamers, 20h, Laboratório 512")
+  
+// }else if(palestra==3){
+//   alert ( "JavaScript para leigos, 19h, Laboratório 101")
+   
+// }else if(palestra==4){
+//  alert ("Tópicos avançados de JavaScript, 20h, laboratório 305")
+ 
+// }else if(palestra==5){
+//   alert  ("Vida e carreira, 21h, Auditório")
+  
+// }   
+
+
+// //Ex5.5//////////
+// let media1, media2, mediaFinal
+// media1= Number(prompt("Digite a primeira média: "))
+// media2= Number(prompt("Digite a segunda média: "))
+// mediaFinal= (media1 + media2) / 2
+// if(mediaFinal>=7){
+//    alert("Aprovado com média: "+ mediaFinal)
+// }else{
+//    alert("Aprovado com média: "+ mediaFinal)
+// }
+
+
+// //Ex5.6//////////
+// let pontuação
+// pontuação= Number(prompt("Digite a pontuação: "))
+// if(pontuação<=10){
+//     alert("PÉSSIMO!!!! Não leve a vida tão a sério. Afinal, você nem sairá vivo dela")
+// }else if(pontuação>10 &  pontuação<=100){
+//      alert("SENSACIONATION!!!! Grandes coisas nunca vêm da zona de conforto ")
+
+// }
