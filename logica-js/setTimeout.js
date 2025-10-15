1e75b2b538400b26e7a748b9dacb60e656fd15ad
GIOVANA MARQUES VERÃO <giovana_verao@estudante.sesisenai.org.br>
2025-10-14 21:56:02 -0300

    set.js

---
+++ logica-js/setTimeout.js
@@ -0,0 +1,21 @@
+// setTimeout(()=>{
+//     console.log("Execute depois de 3 segundos.")
+// },3000)
+
+function saudacao(){
+console.log("Olá,")
+}
+
+function imprimeDado(){
+    console.log("imprimir")
+}
+
+setTimeout(imprimeDado,5000)
+saudacao()
+
+
+function soma(){
+    console.log(10+7)
+}
+setTimeout(soma,7000)
+
