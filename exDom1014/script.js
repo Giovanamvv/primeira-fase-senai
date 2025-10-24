c9789ec453987b72581c9dabe1fae53f932b79e7
GIOVANA MARQUES VERÃO <giovana_verao@estudante.sesisenai.org.br>
2025-10-23 21:52:20 -0300

    ex1014Dom

---
+++ exDom1014/script.js
@@ -0,0 +1,11 @@
+let distancia
+
+function resultadoConsumo(){
+    // console.log("até aqui deu");
+usuario = document.getElementById("inputDistancia").value
+document.getElementById("inputDistancia").value = ``
+
+document.getElementById("resultado").innerHTML = "km/l: " + distancia
+
+
+}
